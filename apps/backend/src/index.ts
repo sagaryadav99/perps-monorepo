ad3602b6875f3b2ab49ab34ed5be31b4d91e5875
sagar yadav <sagarocean688@gmail.com>
2026-08-25 02:47:04 +0530

    cors fix

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -10,7 +10,7 @@ const PORT = Number(process.env.PORT) || 3000;
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   }),
 );
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  server: {
+    allowedHosts: ["perps.mindraw.in"],
+    hmr: {
+      protocol: "wss",
+      host: "perps.mindraw.in",
+      clientPort: 443,
+    },
+  },
 });
